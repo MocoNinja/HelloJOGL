4611e2327ae2032179d029d62ea8fd9c179c8672
MocoNinja <javiergonzaleztello@gmail.com>
2020-04-15 20:40:49 +0200

    Init repo
    Signed-off-by: MocoNinja <javiergonzaleztello@gmail.com>

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# Readme
+
+## Comandos útils
+
+* Empaquetar un *über*jar: ```mvn package```
+* Lanzar el programa: ```mvn exec:java```
---
+++ src/main/java/es/javier/learning/gfx/App.java
@@ -0,0 +1,13 @@
+package es.javier.learning.gfx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
