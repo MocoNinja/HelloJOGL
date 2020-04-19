897b247531fb27347974287cdde32ff45ea77a0e
MocoNinja <javiergonzaleztello@gmail.com>
2020-04-19 19:22:20 +0200

    blab
    Signed-off-by: MocoNinja <javiergonzaleztello@gmail.com>

---
+++ generate.sh
@@ -0,0 +1,1 @@
+mvn archetype:generate -DgroupId=es.javier.learning.gfx -DartifactId=hello-jogl -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
--- src/main/java/es/javier/learning/gfx/glCanvasLearn/RandomTriangleCanvas.java
+++ src/main/java/es/javier/learning/gfx/glCanvasLearn/RandomTriangleCanvas.java
@@ -1,5 +1,9 @@
 package es.javier.learning.gfx.glCanvasLearn;
 
+import com.jogamp.newt.event.KeyEvent;
+import com.jogamp.newt.event.KeyListener;
+import com.jogamp.newt.event.MouseEvent;
+import com.jogamp.newt.event.MouseListener;
 import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.GLAutoDrawable;
 
@@ -7,7 +11,7 @@ import es.javier.learning.gfx.model.CustomCanvas;
 import es.javier.learning.gfx.model.Triangle;
 import es.javier.learning.gfx.utils.Normalizator;
 
-public class RandomTriangleCanvas extends CustomCanvas {
+public class RandomTriangleCanvas extends CustomCanvas implements KeyListener, MouseListener {
 
     Triangle triangle;
     private final Normalizator norm;
@@ -49,6 +53,56 @@ public class RandomTriangleCanvas extends CustomCanvas {
     @Override
     public void update() {
         // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        System.out.println("KEY PRESSED");
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseWheelMoved(MouseEvent e) {
 
     }
 }
